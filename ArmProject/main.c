35ae6b83bacec4e53307a8a21b5155110f5353ef
wwang13 <191373193@qq.com>
2016-04-22 11:20:09 +0800

    complete main.c file

--- ArmProject/main.c
+++ ArmProject/main.c
@@ -22,13 +22,36 @@ int main(void)
 	WORD wErrCode = 0;
 	
 	WORD dataLen = 128;
-	WORD Len = 32;
-	BYTE strOutput[32] = {0};
-
-	
+	WORD Len = 117;
+	BYTE strOutput[117] = {0};
+	BYTE strOInput[117] = {0};
+	char temp[117] = {0};
+	char* strTemp1 = (char*)malloc(117);
+	char* strTemp2 = strTemp1;
+	char* tag = "0000";
+	int i = 0;
+	int size = 0;
 	wErrCode = rsa_pri(0x1111, InOutBuf, dataLen, strOutput,&Len, MODE_DECODE);
 	
-	wErrCode = rsa_pri(0x1111, strOutput, Len, InOutBuf, &dataLen, MODE_ENCODE);
+	while (strOutput[i++] != 0)
+	{
+		size++;
+	}
+	memcpy(temp, strOutput, size);
+	for(i = 0; i < size + 4;i++)
+	{
+		if(i < size)
+		{
+			*strTemp1++ = temp[i];
+		}
+		else{
+			*strTemp1++ = *tag++;
+		}
+	}
+	
+	memcpy(strOInput, strTemp2,size + 4);
+	
+	wErrCode = rsa_pri(0x1111, strOInput, Len, InOutBuf, &dataLen, MODE_ENCODE);
 
 	return wErrCode;
 }
