d1e18167cebf4ec6f428015320a9ee45955f9575
wwang13 <191373193@qq.com>
2016-04-22 16:19:34 +0800

    New pro_RSAKey.cpp

--- Generater/pro_RSAKey.cpp
+++ Generater/pro_RSAKey.cpp
@@ -10,14 +10,18 @@ int _tmain(int argc, _TCHAR* argv[])
 	DWORD dwRet = 0;
 	int nCount = 0;
 	int i = 0;
-	char AdminPin[] = "FFFFFFFFFFFFFFFF";
-	int nRemainCount = 0;
+	char AdminPin[32] = { 0 };
+	int KeyLen = 32;
+	int nRemainCount = 255;
 
 	DONGLE_HANDLE hDongle = NULL;
 	RSA_PUBLIC_KEY  rsaPub;
 	RSA_PRIVATE_KEY rsaPri;
 	PRIKEY_FILE_ATTR priAttr;
 	WORD wPriID = 0x1111;
+
+	printf("Please Input AdminPin:");
+	scanf_s("%s", &AdminPin, KeyLen);
 
 	dwRet = Dongle_Enum(NULL, &nCount);
 	printf("Enum %d Dongle ARM. \n", nCount);
@@ -26,14 +30,18 @@ int _tmain(int argc, _TCHAR* argv[])
 	printf("Open Dongle ARM. Return : 0x%08X . \n", dwRet);
 
 	dwRet = Dongle_VerifyPIN(hDongle, FLAG_ADMINPIN, AdminPin, &nRemainCount);
-	printf("Verify Admin PIN. Return: 0x%08X\n", dwRet);
-
+	if (dwRet != DONGLE_SUCCESS)
+	{
+		printf("Verify Admin PIN. failure Return: 0x%08X\n", dwRet);
+		return 0;
+	}
+		
 	priAttr.m_Size = 1024;
 	priAttr.m_Type = FILE_PRIKEY_RSA;
 	priAttr.m_Lic.m_Count = 0xFFFFFFFF;
 	priAttr.m_Lic.m_IsDecOnRAM = FALSE;
 	priAttr.m_Lic.m_IsReset = FALSE;
-	priAttr.m_Lic.m_Priv = 0;
+	priAttr.m_Lic.m_Priv = 2;
 
 	dwRet = Dongle_CreateFile(hDongle, FILE_PRIKEY_RSA, wPriID, (void*)&priAttr);
 	printf("Create RSA private key file. Return: 0x%08X\n", dwRet);
